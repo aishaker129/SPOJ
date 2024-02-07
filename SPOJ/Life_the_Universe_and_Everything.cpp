e9c26795a5b3edf03747f3b4e1886f796e486e0f
Md Akhlakul Islam <109545500+aishaker129@users.noreply.github.com>
2024-02-08 00:24:06 +0600

    Add files via upload

---
+++ SPOJ/Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,26 @@
+/*
+    Alhamdulillah For Everything
+    Author --> Md. Akhlakul Islam
+    Date --> 07/02/2024 
+*/ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+int main(){
+    optimize();
+    while(true){
+        int n; cin>>n;
+        if(n==42){
+            break;
+        }
+        else{
+            cout<<n<<endl;
+        }
+    }
+
+    return 0;
+}
